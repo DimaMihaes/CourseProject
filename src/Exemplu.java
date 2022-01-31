17486d13eb490c7d0be5c04d5b3a4c7dbda54d33
Dima Mihaes <dima.mihaes@gmail.com>
2022-01-31 16:38:05 +0200

    mesaj

---
+++ src/Exemplu.java
@@ -0,0 +1,7 @@
+public class Exemplu {
+    String testValue;
+
+    Exemplu(String value ){
+        this.testValue=value;
+    }
+}
---
+++ src/NewClass.java
@@ -0,0 +1,9 @@
+public class NewClass {
+    public static void main (String[] args) {
+       Exemplu exemplu=new Exemplu("Dima");
+       System.out.println(exemplu.testValue);
+    }
+    public  static  void  printName(String name){
+        System.out.println(name);
+    }
+}
