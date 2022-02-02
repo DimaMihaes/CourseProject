a774defc5d5a804936dfde289376a22f927f64c2
Dima Mihaes <dima.mihaes@gmail.com>
2022-02-02 16:53:10 +0200

    lesson4

---
+++ src/edu/tekwil/course/lesson4/MinutesConverter.java
@@ -0,0 +1,13 @@
+package edu.tekwil.course.lesson4;
+
+public class MinutesConverter {
+    public static void main (String[] args){
+        long minutes = 525948L ;
+        double days = minutes / 1440;
+        double years = days / 365;
+        System.out.println("Minute in zile: "+days);
+        System.out.println("Minute in ani: "+years);
+
+    }
+
+}
--- src/edu/tekwil/course/lesson4/TaxCalculator.java
+++ src/edu/tekwil/course/lesson4/TaxCalculator.java
@@ -1,4 +1,32 @@
 package edu.tekwil.course.lesson4;
 
+import java.util.Scanner;
+
 public class TaxCalculator {
+    public static void main (String[] args) {
+
+        System.out.println("Introduceti pretul: ");
+        Scanner a=new Scanner(System.in);
+        double pret = a.nextDouble() ;
+
+
+        System.out.println("Introduceti taxa:" );
+        Scanner b=new Scanner(System.in);
+        double tax= b.nextDouble() ;
+
+
+        System.out.println("Introduceti cantitatea: ");
+        Scanner c=new Scanner(System.in);
+        int quantity = c.nextInt(); 
+
+        a.close();
+        b.close();
+        c.close();
+
+        double total ;
+        total= pret * tax * quantity ;
+        System.out.println("Total cost with tax is: " +total);
+
+
+    }
 }
