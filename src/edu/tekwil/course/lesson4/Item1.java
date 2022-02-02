f4473d83dd4a801a981a949e368015c14679ffa4
Dima Mihaes <dima.mihaes@gmail.com>
2022-02-02 16:05:17 +0200

    Lesson4

---
+++ src/edu/tekwil/course/lesson4/Item1.java
@@ -0,0 +1,17 @@
+package edu.tekwil.course.lesson4;
+
+public class Item1 {
+    public static void main (String[] args) {
+        long a = 7811080815L;
+        int b= 55235358;
+        char c = 'A' ;
+        boolean d= true ;
+        float e = -3.14159F;
+        System.out.println("Current World Population: "+a);
+        System.out.println("Cars Produced This Year: "+b);
+        System.out.println("First Letter In Alphabet: "+c);
+        System.out.println("Java Is Amazing: "+d);
+        System.out.println("Pi is: " +e);
+
+    }
+}
